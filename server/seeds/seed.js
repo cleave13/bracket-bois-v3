6ef5b248411f1ae4330c8c9f22809c430a12185c
cleave13 <94774869+cleave13@users.noreply.github.com>
2022-06-04 12:46:46 -0600

    added server.js file

---
+++ server/seeds/seed.js
@@ -0,0 +1,13 @@
+const db = require('../config/connection');
+const { Course } = require('../models');
+
+const courseData = require('./courseData.json');
+
+db.once('open', async () => {
+  await Course.deleteMany({});
+
+  await Course.insertMany(courseData);
+
+  console.log('Courses seeded!');
+  process.exit(0);
+});
