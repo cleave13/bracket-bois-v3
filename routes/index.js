4c1eab01779b6abbcd85a24d529d73b84db155b3
cleave13 <94774869+cleave13@users.noreply.github.com>
2022-06-04 13:02:08 -0600

    cleaned unused folders and files

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const apiRoutes = require('./api');
-
-router.use('/api', apiRoutes);
-
-// Catch-all route in case the user tries to access something other than the API route
-router.use((req, res) => res.send('This app only supports api routes. Be sure the request is sent to {PORT}/api/'));
-
-module.exports = router;
