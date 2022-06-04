c376a43b7c35fe0f10b5eb9a424aa4801409cfb9
christierney3 <chris.tierney3@gmail.com>
2022-06-04 13:29:31 -0600

    changed packages, added into resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -2,6 +2,11 @@ const { Course, Hole, Round } = require('../models');
 
 const resolvers = {
     Query: {
-        
-    }
+      users: async () => {
+        return User.find().populate('rounds');
+      },
+      user: async (parent, { username }) => {
+        return User.findOne({ username }).populate('thoughts');
+      },
+
 }
