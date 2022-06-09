c4749c0a11289b6b3b908c072c6ed7ad3fb854bd
cleave13 <94774869+cleave13@users.noreply.github.com>
2022-06-08 20:17:11 -0600

    fixed broken references

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { Navigate, useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 
-import ThoughtForm from '../components/ThoughtForm';
-import ThoughtList from '../components/ThoughtList';
 import Navigation from '../components/Nav';
 
 import { QUERY_USER, QUERY_ME } from '../utils/queries';
@@ -43,23 +41,6 @@ const Profile = () => {
         <h2 className="col-12 col-md-10 bg-dark text-light p-3 mb-5">
           Viewing {userParam ? `${user.username}'s` : 'your'} profile.
         </h2>
-
-        <div className="col-12 col-md-10 mb-5">
-          <ThoughtList
-            thoughts={user.thoughts}
-            title={`${user.username}'s thoughts...`}
-            showTitle={false}
-            showUsername={false}
-          />
-        </div>
-        {!userParam && (
-          <div
-            className="col-12 col-md-10 mb-3 p-3"
-            style={{ border: '1px dotted #1a1a1a' }}
-          >
-            <ThoughtForm />
-          </div>
-        )}
       </div>
     </div>
   );
